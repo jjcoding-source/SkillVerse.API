6dc7867fe10efa0a5fad85aea38d1710a77700fc
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 13:15:14 +0530

    Add DashboardStatsDto for admin dashboard statistics

---
+++ SkillVerse.API/DTOs/Admin/DashboardStatsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillVerse.API.DTOs.Admin
+{
+    public class DashboardStatsDto
+    {
+        public int TotalCustomers { get; set; }
+        public int TotalWorkers { get; set; }
+        public int TotalBookings { get; set; }
+        public int PendingBookings { get; set; }
+    }
+}
