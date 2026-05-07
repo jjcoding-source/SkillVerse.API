129aa7dc3949d3e5543ad951252eb86584d3e470
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 12:56:40 +0530

    Implement ServiceController for service catalog endpoints

---
+++ SkillVerse.API/Controllers/ServiceController.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using SkillVerse.API.Services.Interfaces;
+
+namespace SkillVerse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly IServiceService _serviceService;
+
+        public ServiceController(IServiceService serviceService)
+        {
+            _serviceService = serviceService;
+        }
+
+        [HttpGet("categories")]
+        public async Task<IActionResult> GetCategories()
+        {
+            var response = await _serviceService.GetAllCategoriesAsync();
+            return Ok(response);
+        }
+
+        [HttpGet("services")]
+        public async Task<IActionResult> GetServices([FromQuery] int? categoryId = null)
+        {
+            var response = await _serviceService.GetServicesByCategoryAsync(categoryId);
+            return Ok(response);
+        }
+    }
+}
