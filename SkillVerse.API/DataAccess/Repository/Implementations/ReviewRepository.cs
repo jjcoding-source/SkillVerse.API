c911e8d9d19587d804ab400f90704cbe2ab8a695
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 17:06:41 +0530

    Implement ReviewRepository for handling review creation operations

---
+++ SkillVerse.API/DataAccess/Repository/Implementations/ReviewRepository.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Data;
+using Microsoft.Data.SqlClient;
+using SkillVerse.API.DataAccess.Repository.Interfaces;
+using SkillVerse.API.DTOs.Review;
+
+namespace SkillVerse.API.DataAccess.Repository.Implementations
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly IBaseRepository _baseRepository;
+
+        public ReviewRepository(IBaseRepository baseRepository)
+        {
+            _baseRepository = baseRepository;
+        }
+
+        public Task<int> AddReviewAsync(int customerId, ReviewCreateDto dto)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@BookingID", dto.BookingID),
+                new SqlParameter("@CustomerID", customerId),
+                new SqlParameter("@Rating", dto.Rating),
+                new SqlParameter("@Comment", dto.Comment ?? (object)DBNull.Value)
+            };
+
+            return _baseRepository.ExecuteNonQueryAsync("sp_AddReview", parameters);
+        }
+    }
+}
