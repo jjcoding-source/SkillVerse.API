ab2b4479e86b139f0cd1bd98a37d799fda0f5f77
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 13:19:35 +0530

    Add IAdminService interface for admin operations

---
+++ SkillVerse.API/Services/Interfaces/IAdminService.cs
@@ -0,0 +1,12 @@
+﻿
+using SkillVerse.API.DTOs.Admin;
+using SkillVerse.API.DTOs.Common;
+
+namespace SkillVerse.API.Services.Interfaces
+{
+    public interface IAdminService
+    {
+        Task<ApiResponse<DashboardStatsDto>> GetDashboardStatsAsync();
+        Task<ApiResponse<bool>> ToggleUserStatusAsync(int userId, bool isActive);
+    }
+}
