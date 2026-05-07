64b79c98de31372c2d579ff54363e41e0f6767db
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 16:58:17 +0530

    Implement ServiceRepository for service and category retrieval operations

---
+++ SkillVerse.API/DataAccess/Repository/Implementations/ServiceRepository.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Data;
+using Microsoft.Data.SqlClient;
+using SkillVerse.API.DataAccess.Repository.Interfaces;
+
+namespace SkillVerse.API.DataAccess.Repository.Implementations
+{
+    public class ServiceRepository : IServiceRepository
+    {
+        private readonly IBaseRepository _baseRepository;
+
+        public ServiceRepository(IBaseRepository baseRepository)
+        {
+            _baseRepository = baseRepository;
+        }
+
+        public Task<DataTable> GetAllCategoriesAsync()
+        {
+            return _baseRepository.ExecuteDataTableAsync("sp_GetAllServiceCategories");
+        }
+
+        public Task<DataTable> GetServicesByCategoryAsync(int? categoryId)
+        {
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@CategoryID", categoryId ?? (object)DBNull.Value)
+            };
+
+            return _baseRepository.ExecuteDataTableAsync("sp_GetServicesByCategory", parameters);
+        }
+    }
+}
