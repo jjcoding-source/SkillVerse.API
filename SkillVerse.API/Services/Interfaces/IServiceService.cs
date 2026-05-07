df3152dea662679edf605a70462cfd09ab3dd091
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 12:53:17 +0530

    Add IServiceService interface for service catalog operations

---
+++ SkillVerse.API/Services/Interfaces/IServiceService.cs
@@ -0,0 +1,12 @@
+﻿
+using SkillVerse.API.DTOs.Common;
+using SkillVerse.API.DTOs.Service;
+
+namespace SkillVerse.API.Services.Interfaces
+{
+    public interface IServiceService
+    {
+        Task<ApiResponse<List<ServiceCategoryDto>>> GetAllCategoriesAsync();
+        Task<ApiResponse<List<ServiceDto>>> GetServicesByCategoryAsync(int? categoryId = null);
+    }
+}
