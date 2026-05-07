8958e182cc80a4a13764a85aa09f52211d601d7c
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 12:06:18 +0530

    Add IUserService interface for user profile operations

---
+++ SkillVerse.API/Services/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿
+using SkillVerse.API.DTOs.Common;
+using SkillVerse.API.DTOs.User;
+
+namespace SkillVerse.API.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<ApiResponse<UserProfileDto>> GetUserProfileAsync(int userId);
+        Task<ApiResponse<bool>> UpdateUserProfileAsync(int userId, UserProfileDto dto);
+    }
+}
