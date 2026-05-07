da1aea990a31f683e208f06a2a749027d518d8b4
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 16:07:14 +0530

    Add IBaseRepository interface for database operation abstraction

---
+++ SkillVerse.API/DataAccess/Repository/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace SkillVerse.API.DataAccess.Repository.Interfaces
+{
+    public interface IBaseRepository
+    {
+        Task<DataTable> ExecuteDataTableAsync(string procedureName, SqlParameter[]? parameters = null);
+        Task<int> ExecuteNonQueryAsync(string procedureName, SqlParameter[]? parameters = null);
+        Task<object?> ExecuteScalarAsync(string procedureName, SqlParameter[]? parameters = null);
+    }
+}
