2f2380e53f0ff4204338aa688b3e7f74be3457a7
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 13:26:03 +0530

    Add SendMessageDto for sending chat messages

---
+++ SkillVerse.API/DTOs/Chat/SendMessageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SkillVerse.API.DTOs.Chat
+{
+    public class SendMessageDto
+    {
+        public int BookingID { get; set; }
+        public string MessageText { get; set; } = string.Empty;
+    }
+}
