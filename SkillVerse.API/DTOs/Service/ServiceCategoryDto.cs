e502c5bd0d5efe7d6edf48eeb3ef5d032cedea1c
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 12:47:48 +0530

    Add ServiceCategoryDto for service category responses

---
+++ SkillVerse.API/DTOs/Service/ServiceCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SkillVerse.API.DTOs.Service
+{
+    public class ServiceCategoryDto
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string? Icon { get; set; }
+    }
+}
