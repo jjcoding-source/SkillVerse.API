e127fbdb5dd5771dab6b735cc3a616299f23139c
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 13:07:03 +0530

    Add IReviewService interface for review operations

--- SkillVerse.API/Services/Interfaces/IReviewService.cs
+++ SkillVerse.API/Services/Interfaces/IReviewService.cs
@@ -1,6 +1,12 @@
-﻿namespace SkillVerse.API.Services.Interfaces
+﻿
+using SkillVerse.API.DTOs.Common;
+using SkillVerse.API.DTOs.Review;
+
+namespace SkillVerse.API.Services.Interfaces
 {
     public interface IReviewService
     {
+        Task<ApiResponse<bool>> AddReviewAsync(int customerId, ReviewCreateDto dto);
+        Task<ApiResponse<List<ReviewDto>>> GetWorkerReviewsAsync(int workerId);
     }
 }
