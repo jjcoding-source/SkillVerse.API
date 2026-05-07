593db59fec03b78fcbc2169a498952b9322ab2ba
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 16:12:10 +0530

    Add IUserRepository for user authentication and profile operations

---
+++ SkillVerse.API/DataAccess/Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Data;
+using Microsoft.Data.SqlClient;
+using SkillVerse.API.DTOs.Auth;
+using SkillVerse.API.DTOs.User;
+
+namespace SkillVerse.API.DataAccess.Repository.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<DataTable> LoginUserAsync(string email, string passwordHash);
+        Task<int> CreateUserAsync(RegisterDto dto);
+        Task<DataTable> GetUserProfileAsync(int userId);
+        Task<int> UpdateUserProfileAsync(int userId, UserProfileDto dto);
+    }
+}
