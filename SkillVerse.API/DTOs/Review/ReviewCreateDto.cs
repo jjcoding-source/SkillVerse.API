1c3f95f4519a99266d76053808bf4933dbf71d4d
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 13:00:58 +0530

    Add ReviewCreateDto for review creation requests

---
+++ SkillVerse.API/DTOs/Review/ReviewCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SkillVerse.API.DTOs.Review
+{
+    public class ReviewCreateDto
+    {
+        public int BookingID { get; set; }
+        public int Rating { get; set; }          
+        public string? Comment { get; set; }
+    }
+}
