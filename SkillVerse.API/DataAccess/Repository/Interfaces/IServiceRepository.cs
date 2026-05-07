c6dada3eb602d7ba4c8743a0f73892f3d82a2d54
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 16:57:48 +0530

    Add IServiceRepository for service and category data access

---
+++ SkillVerse.API/DataAccess/Repository/Interfaces/IServiceRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Data;
+
+namespace SkillVerse.API.DataAccess.Repository.Interfaces
+{
+    public interface IServiceRepository
+    {
+        Task<DataTable> GetAllCategoriesAsync();
+        Task<DataTable> GetServicesByCategoryAsync(int? categoryId);
+    }
+}
