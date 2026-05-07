0b3e67e1ddaaa359b9a70db0cb1896cbbbc326df
jjcoding-source <jebinjacob62@gmail.com>
2026-05-07 16:07:41 +0530

    Implement BaseRepository as wrapper over DbHelper for data access layer

---
+++ SkillVerse.API/DataAccess/Repository/Implementations/BaseRepository.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Data;
+using Microsoft.Data.SqlClient;
+using SkillVerse.API.DataAccess.Repository.Interfaces;
+
+namespace SkillVerse.API.DataAccess.Repository.Implementations
+{
+    public class BaseRepository : IBaseRepository
+    {
+        private readonly DbHelper _dbHelper;
+
+        public BaseRepository(DbHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+
+        public Task<DataTable> ExecuteDataTableAsync(string procedureName, SqlParameter[]? parameters = null)
+        {
+            return _dbHelper.ExecuteDataTableAsync(procedureName, parameters);
+        }
+
+        public Task<int> ExecuteNonQueryAsync(string procedureName, SqlParameter[]? parameters = null)
+        {
+            return _dbHelper.ExecuteNonQueryAsync(procedureName, parameters);
+        }
+
+        public Task<object?> ExecuteScalarAsync(string procedureName, SqlParameter[]? parameters = null)
+        {
+            return _dbHelper.ExecuteScalarAsync(procedureName, parameters);
+        }
+    }
+}
