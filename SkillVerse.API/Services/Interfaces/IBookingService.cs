4dcb5ecb6784f19f8939c1e08707647cc1aabf89
jjcoding-source <jebinjacob62@gmail.com>
2026-05-08 13:02:53 +0530

    feat(booking-service): add booking details accept and reject service methods

--- SkillVerse.API/Services/Interfaces/IBookingService.cs
+++ SkillVerse.API/Services/Interfaces/IBookingService.cs
@@ -1,6 +1,8 @@
 ﻿
+
 using SkillVerse.API.DTOs.Booking;
 using SkillVerse.API.DTOs.Common;
+using System.Data;
 
 namespace SkillVerse.API.Services.Interfaces
 {
@@ -9,6 +11,11 @@ namespace SkillVerse.API.Services.Interfaces
         Task<ApiResponse<int>> CreateBookingAsync(int customerId, BookingCreateDto dto);
         Task<ApiResponse<List<BookingResponseDto>>> GetCustomerBookingsAsync(int customerId, string? status = null);
         Task<ApiResponse<List<BookingResponseDto>>> GetWorkerBookingsAsync(int workerId, string? status = null);
+        Task<ApiResponse<DataTable>> GetBookingDetailsByIdAsync(int bookingId);
+
         Task<ApiResponse<bool>> UpdateBookingStatusAsync(int bookingId, int? workerId, string newStatus, string? cancelReason = null);
+
+        Task<ApiResponse<bool>> AcceptBookingAsync(int bookingId, int workerId);
+        Task<ApiResponse<bool>> RejectBookingAsync(int bookingId, string cancelReason);
     }
 }
